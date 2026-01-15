8261a71e18029776cd4d39c55b6b1c8ef6801a24
eekatzee18 <kuptsova.98@list.ru>
2026-01-15 11:57:08 +0400

    resolved mistakes

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -1,7 +1,14 @@
-import {HealthHeroes} from '../basic';
+import HealthHeroes from '../basic';
 
-test('checkup health', () => {
-  const result = HealthHeroes({name: 'Маг', health: 90});
-
-  expect(result).toBe("healthy");
+test('checkup health > 50', () => {
+  const result = HealthHeroes({ name: 'Маг', health: 90 });
+  expect(result).toBe('healthy');
+});
+test('checkup health < 50 и > 15', () => {
+  const result = HealthHeroes({ name: 'Маг', health: 45 });
+  expect(result).toBe('wounded');
+});
+test('checkup health < 15', () => {
+  const result = HealthHeroes({ name: 'Маг', health: 10 });
+  expect(result).toBe('critical');
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,17 +1,9 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+export default function HealthHeroes(heroe) {
+  if (heroe.health > 50) {
+    return 'healthy';
   }
-  return result;
-}
-
-export function HealthHeroes(heroe){
-  if(heroe.health > 50) {
-    return "healthy";
-  } else if (heroe.health <= 50 && heroe.health >=15) {
-    return "wounded";
-  }else {
-    return "critical";
+  if (heroe.health >= 15) {
+    return 'wounded';
   }
+  return 'critical';
 }
